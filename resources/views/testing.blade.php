385a4b60dce15fe5eec74aa8ca25bf7c76f793ed
windisiswandi <windisiswandi89@gmail.com>
2024-03-20 23:07:00 +0800

    setup tailwindcss

---
+++ resources/views/testing.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container p-10 border border-r-4 border-teal-900 border-8 mr-auto mt-10">
+        <h1 class="text-slate-900 underline text-center font-bold text-3xl">Hello Wordl</h1>
+        <div class="flex mt-5">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At dignissimos laudantium assumenda nulla, eum cumque iure maxime corrupti? Minus labore nesciunt praesentium velit blanditiis fugit dicta aperiam ipsum recusandae? Voluptates, odio deleniti ratione at possimus, dignissimos sed ea unde aliquid doloremque, ipsum consectetur? Atque alias non magni blanditiis rerum quo doloremque laudantium! Accusantium repellat dolor, eius quis earum ducimus doloribus nisi repudiandae alias excepturi harum consequatur odio, libero at autem, cumque beatae sed. Consequuntur perspiciatis ducimus repellendus quasi officia maxime accusantium eum, rem deserunt deleniti maiores nobis ipsa dolorem! Incidunt magnam praesentium soluta, dolorum explicabo reprehenderit porro! Dolores, aut nisi?</p>
+        </div>
+    </div>
+</body>
+</html>
