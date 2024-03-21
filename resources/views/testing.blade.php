54ba34b0867792376b9915ab1664e186bfdff67b
windisiswandi <windisiswandi89@gmail.com>
2024-03-21 12:59:38 +0800

    create landing page index

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="container p-10 border border-r-4 border-teal-900 border-8 mr-auto mt-10">
+    <div class="container p-10 border-teal-900 border-8 mr-auto mt-10 mx-auto">
         <h1 class="text-slate-900 underline text-center font-bold text-3xl">Hello Wordl</h1>
         <div class="flex mt-5">
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At dignissimos laudantium assumenda nulla, eum cumque iure maxime corrupti? Minus labore nesciunt praesentium velit blanditiis fugit dicta aperiam ipsum recusandae? Voluptates, odio deleniti ratione at possimus, dignissimos sed ea unde aliquid doloremque, ipsum consectetur? Atque alias non magni blanditiis rerum quo doloremque laudantium! Accusantium repellat dolor, eius quis earum ducimus doloribus nisi repudiandae alias excepturi harum consequatur odio, libero at autem, cumque beatae sed. Consequuntur perspiciatis ducimus repellendus quasi officia maxime accusantium eum, rem deserunt deleniti maiores nobis ipsa dolorem! Incidunt magnam praesentium soluta, dolorum explicabo reprehenderit porro! Dolores, aut nisi?</p>
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,81 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    $notes = [
+        [
+            'title' => 'Pertemuan dengan Klien Baru',
+            'date' => '2024-03-20',
+            'note' => 'Diskusi tentang proyek baru.',
+            'slug' => 'pertemuan-dengan-klien-baru'
+        ],
+        [
+            'title' => 'Rapat Tim',
+            'date' => '2024-03-18',
+            'note' => 'Review kemajuan proyek.',
+            'slug' => 'rapat-tim'
+        ],
+        [
+            'title' => 'Pengiriman Barang',
+            'date' => '2024-03-15',
+            'note' => 'Pengiriman ke pelanggan.',
+            'slug' => 'pengiriman-barang'
+        ],
+        [
+            'title' => 'Pembayaran Tagihan',
+            'date' => '2024-03-12',
+            'note' => 'Pembayaran tagihan ke pemasok.',
+            'slug' => 'pembayaran-tagihan'
+        ],
+        [
+            'title' => 'Pelatihan Karyawan',
+            'date' => '2024-03-10',
+            'note' => 'Pelatihan karyawan baru.',
+            'slug' => 'pelatihan-karyawan'
+        ],
+        [
+            'title' => 'Update Perangkat Lunak',
+            'date' => '2024-03-08',
+            'note' => 'Update sistem ke versi terbaru.',
+            'slug' => 'update-perangkat-lunak'
+        ],
+        [
+            'title' => 'Pemasaran Online',
+            'date' => '2024-03-05',
+            'note' => 'Kampanye iklan online.',
+            'slug' => 'pemasaran-online'
+        ],
+        [
+            'title' => 'Diskusi Proyek',
+            'date' => '2024-03-02',
+            'note' => 'Diskusi tentang kemajuan proyek.',
+            'slug' => 'diskusi-proyek'
+        ],
+        [
+            'title' => 'Meeting dengan Investor',
+            'date' => '2024-02-28',
+            'note' => 'Presentasi kepada calon investor.',
+            'slug' => 'meeting-dengan-investor'
+        ],
+        [
+            'title' => 'Pertemuan Penjualan',
+            'date' => '2024-02-25',
+            'note' => 'Pertemuan dengan calon pelanggan.',
+            'slug' => 'pertemuan-penjualan'
+        ]
+    ];
+
+
+    return view('index', ["notes" => $notes]);
 });
 
 Route::get('/testing', function () {
-    return view('testing');
+    return view("testing");
+});
+
+Route::get("/note/{nama}", function ($note) {
+    return "ini note $note";
 });
 
