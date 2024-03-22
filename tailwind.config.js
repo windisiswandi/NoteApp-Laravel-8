609787e01936f431b0890d59f03ae1448dd80876
windisiswandi <windisiswandi89@gmail.com>
2024-03-22 10:57:51 +0800

    feat: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./resources/**/*.{blade.php,vue,js,html}",
-    ],
+    content: ["./resources/**/*.{blade.php,vue,js,html}"],
     theme: {
+        screens: {
+            "sm": "640px",
+            "md": "768px",
+            "lg": "1024px",
+            "xl": "1280px",
+            "2xl": "1440px",
+            "3xl": "1600px",
+        },
         extend: {},
+        container: { center: true },
     },
     plugins: [],
 };
-
