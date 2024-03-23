150b1c9540e48f7f04520b7d56f480e6620ff313
windisiswandi <windisiswandi89@gmail.com>
2024-03-23 20:56:26 +0800

    feat: update NoteController

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -4,83 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Note;
 
 class NoteController extends Controller
 {
     public function index() {
-        $notes = [
-            [
-                'title' => 'Pertemuan dengan Klien Baru',
-                'date' => '2024-03-20',
-                'note' => 'Diskusi tentang proyek baru.',
-                'slug' => 'pertemuan-dengan-klien-baru'
-            ],
-            [
-                'title' => 'Rapat Tim',
-                'date' => '2024-03-18',
-                'note' => 'Review kemajuan proyek.',
-                'slug' => 'rapat-tim'
-            ],
-            [
-                'title' => 'Pengiriman Barang',
-                'date' => '2024-03-15',
-                'note' => 'Pengiriman ke pelanggan.',
-                'slug' => 'pengiriman-barang'
-            ],
-            [
-                'title' => 'Pembayaran Tagihan',
-                'date' => '2024-03-12',
-                'note' => 'Pembayaran tagihan ke pemasok.',
-                'slug' => 'pembayaran-tagihan'
-            ],
-            [
-                'title' => 'Pelatihan Karyawan',
-                'date' => '2024-03-10',
-                'note' => 'Pelatihan karyawan baru.',
-                'slug' => 'pelatihan-karyawan'
-            ],
-            [
-                'title' => 'Update Perangkat Lunak',
-                'date' => '2024-03-08',
-                'note' => 'Update sistem ke versi terbaru.',
-                'slug' => 'update-perangkat-lunak'
-            ],
-            [
-                'title' => 'Pemasaran Online',
-                'date' => '2024-03-05',
-                'note' => 'Kampanye iklan online.',
-                'slug' => 'pemasaran-online'
-            ],
-            [
-                'title' => 'Diskusi Proyek',
-                'date' => '2024-03-02',
-                'note' => 'Diskusi tentang kemajuan proyek.',
-                'slug' => 'diskusi-proyek'
-            ],
-            [
-                'title' => 'Meeting dengan Investor',
-                'date' => '2024-02-28',
-                'note' => 'Presentasi kepada calon investor.',
-                'slug' => 'meeting-dengan-investor'
-            ],
-            [
-                'title' => 'Pertemuan Penjualan',
-                'date' => '2024-02-25',
-                'note' => 'Pertemuan dengan calon pelanggan.',
-                'slug' => 'pertemuan-penjualan'
-            ],
-            [
-                'title' => 'Pertemuan Penjualan',
-                'date' => '2024-02-25',
-                'note' => 'Pertemuan dengan calon pelanggan.',
-                'slug' => 'pertemuan-penjualan'
-            ]
-        ];
-
-
         return view('index', [
             "title" => "Note APP | Dashboard",
-            "notes" => $notes
+            "notes" => Note::get_all()
+        ]);
+    }
+
+    public function find_note($slug) {
+        return view('note_detail', [
+            "title" => "Note APP | Detail",
+            "note" => Note::find($slug)
         ]);
     }
 
