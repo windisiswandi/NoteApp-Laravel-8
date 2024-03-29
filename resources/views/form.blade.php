4970b7b5b4c3289c91822eb7f363d7a64b9e6234
windisiswandi <windisiswandi89@gmail.com>
2024-03-30 00:00:50 +0800

    feat: update views

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,8 +1,9 @@
 @extends('components.layout')
 
 @section('content')
-    <h1 class="text-center font-bold text-3xl">Tambah catatan</h1>
+    
     @if($purpose == 'create')
+    <h1 class="text-center font-bold text-3xl">Tambah catatan</h1>
     <form action="{{route('create-note')}}" method="POST" class="w-1/2 mx-auto mt-10">
         {{ csrf_field() }}
         <div class="mb-4 ">
@@ -18,17 +19,18 @@
         </div>
     </form>
     @else
+    <h1 class="text-center font-bold text-3xl">Update catatan</h1>
     <form action="{{route('update-note')}}" method="post" class="w-1/2 mx-auto mt-10">
         {{ csrf_field() }}
         @method('put')
-        <input type="hidden" name="id" value="{{$note['id']}}">
+        <input type="hidden" name="id" value="{{$note->note_id}}">
         <div class="mb-4 ">
             <label for="title" class="block text-gray-700 font-semibold mb-2">Title:</label>
-            <input type="text" id="title" name="title" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-teal-500" value="{{$note['title']}}" required>
+            <input type="text" id="title" name="title" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-teal-500" value="{{$note->title}}" required>
         </div>
         <div class="mb-4">
             <label for="description" class="block text-gray-700 font-semibold mb-2">Description:</label>
-            <textarea id="description" name="catatan" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-teal-500" rows="4" required>{{$note['catatan']}}</textarea>
+            <textarea id="description" name="catatan" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-teal-500" rows="4" required>{{$note->catatan}}</textarea>
         </div>
         <div class="text-center">
             <button type="submit" class="px-6 py-2 bg-teal-500 text-white rounded hover:bg-teal-600 focus:outline-none focus:bg-teal-600">Update</button>
--- resources/views/note_detail.blade.php
+++ resources/views/note_detail.blade.php
@@ -5,7 +5,7 @@
     <h1 class="text-center font-bold text-3xl">My Note</h1>
     <div class="w-1/2 bg-slate-100 rounded-md p-4 mt-10 mx-auto">
         <div class="title flex justify-between items-center">
-            <h2 class="text-lg font-bold">{{ $note['title'] }}</h2>
+            <h2 class="text-lg font-bold">{{ $note->title }}</h2>
             <div class="relative">
                 <!-- Ikon titik tiga untuk menunjukkan dropdown -->
                 <button class="text-sm font-medium text-gray-700">
@@ -18,14 +18,14 @@
 
                 <div id="menu" class="origin-top-right absolute right-0 mt-2 w-56 rounded-sm shadow-lg bg-white ring-1 ring-black ring-opacity-[0.04] hidden">
                     <!-- Opsi dropdown -->
-                    <a href="{{route('form-update', $note['id'])}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Edit</a>
-                    <a id="delete" data-id="{{$note['id']}}" class="block px-4 py-2 text-sm text-red-700 hover:bg-gray-100 hover:text-red-900" role="menuitem">Delete</a>
+                    <a href="{{route('form-update', $note->note_id)}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Edit</a>
+                    <a href='#' id="delete" data-id="{{$note->note_id}}" class="block px-4 py-2 text-sm text-red-700 hover:bg-gray-100 hover:text-red-900" role="menuitem">Delete</a>
             </div>
 
         </div>
         </div>
-        <p class="text-slate-500 text-[12px] italic">{{date('d/m/Y H:i:s', $note['created_at'])}}</p>
-        <p class="text-slate-800 text-sm mt-4">{{$note['catatan']}}</p>
+        <p class="text-slate-500 text-[12px] italic">{{$note->created_at}}</p>
+        <p class="text-slate-800 text-sm mt-4">{!!$note->catatan!!}</p>
 
         <a href="{{route('dashboard')}}" class="py-1 px-3 rounded-sm text-white bg-blue-500 mt-10 inline-block">Back to home</a>
     </div>
@@ -39,7 +39,7 @@
             event.preventDefault(); // Mencegah tindakan bawaan hyperlink
             const csrf = document.querySelector("input[name='_token']").value
             if (confirm("Anda yakin ingin menghapus?")) {
-                fetch(`{{route('delete-note', $note['id'])}}`, {
+                fetch(`{{route('delete-note', $note->note_id)}}`, {
                     method: "DELETE",
                     headers: {
                         "X-CSRF-TOKEN": csrf
