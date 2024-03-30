dd8e83203a22e44828cfc5caea7b9cdbc5c0e0a0
windisiswandi <windisiswandi89@gmail.com>
2024-03-30 11:53:19 +0800

    feat: update route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 
 /*
@@ -18,13 +19,24 @@ use Illuminate\Http\Request;
 
 
 Route::get('/', [NoteController::class, 'index'])->name('dashboard');
-Route::get("/notes/detail/{note}", [NoteController::class, 'find_note'])->name('detail-note');
-Route::get("/notes/update/{note}", [NoteController::class, 'form_update'])->name('form-update');
+Route::get("/notes/detail/{note:slug}", [NoteController::class, 'find_note'])->name('detail-note');
+Route::get("/notes/update/{note:slug}", [NoteController::class, 'form_update'])->name('form-update');
 Route::get("/notes/add", [NoteController::class, "form_create"])->name("form-tambah");
+Route::get("/categories", [CategoryController::class, "categories"])->name("categories");
+Route::get("/categories/{category:slug}", [CategoryController::class, "categoriesById"])->name("categoriesById");
+
+
+
 
 Route::post("/note", [NoteController::class, 'create'])->name("create-note");
 
+
+
+
 Route::put("/note", [NoteController::class, 'update'])->name("update-note");
+
+
+
 
 Route::delete("/notes/{id}", [NoteController::class, 'delete'])->name('delete-note');
 
